8a4d231ac868ca7fd8400fbc3985addeb5324ab0
HIG <hingjy1997@gmail.com>
2024-12-16 16:16:33 +0900

    refactor : UserPointTable에 @Autowired 어노테이션 추가

--- src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
+++ src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
@@ -2,16 +2,14 @@ package io.hhplus.tdd.point.repository;
 
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.dto.UserPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserPointRepository {
 
-    private final UserPointTable userPointTable;
-
-    public UserPointRepository(UserPointTable userPointTable) {
-        this.userPointTable = userPointTable;
-    }
+    @Autowired
+    private UserPointTable userPointTable;
 
     public UserPoint selectById(long id) {
 
