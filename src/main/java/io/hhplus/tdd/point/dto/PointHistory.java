1ebd98560ce39858f84557ae67203c25c5078815
HIG <hingjy1997@gmail.com>
2024-12-16 15:52:28 +0900

    refactor : Builder 어노테이션 추가

--- src/main/java/io/hhplus/tdd/point/dto/PointHistory.java
+++ src/main/java/io/hhplus/tdd/point/dto/PointHistory.java
@@ -1,7 +1,9 @@
 package io.hhplus.tdd.point.dto;
 
 import io.hhplus.tdd.point.type.TransactionType;
+import lombok.Builder;
 
+@Builder
 public record PointHistory(
         long id,
         long userId,
