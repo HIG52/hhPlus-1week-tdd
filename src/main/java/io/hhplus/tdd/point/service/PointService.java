92fc3c15dda31476d063fae8dd70b3be7e8c2fb7
홍준영 <hongjy1997@naver.com>
2024-12-19 01:35:34 +0900

    feat : 서비스 요청에 대한 동시성 제어

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -8,6 +8,10 @@ import io.hhplus.tdd.point.type.TransactionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Service
 public class PointService {
 
@@ -17,39 +21,70 @@ public class PointService {
     @Autowired
     private PointHistoryRepository pointHistoryRepository;
 
+    //각 ID에 대한 락관리
+    private final ConcurrentHashMap<Long, Lock> lockMap = new ConcurrentHashMap<>();
+
+    private Lock getLock(long id) {
+        return lockMap.computeIfAbsent(id, key -> new ReentrantLock(true)); //공정성 선입선출
+    }
+
     public UserPoint viewPoint(long id) {
-        return userPointRepository.selectById(id);
+        Lock lock = getLock(id);
+        lock.lock(); //락 획득
+        try{
+            return userPointRepository.selectById(id);
+        }finally {
+            lock.unlock(); //락 해제
+        }
+
     }
 
     public UserPoint chargePoint(long id, long point) {
-        long currentPoint = viewPoint(id).point();
-        long chargePoint = point;
-        long updatedPoint = currentPoint + chargePoint;
+        //id별로 락 생성
+        Lock lock = getLock(id);
+        lock.lock(); //락 획득
 
-        chargePointException(chargePoint);
+        try {
+            long currentPoint = viewPoint(id).point();
+            long chargePoint = point;
+            long updatedPoint = currentPoint + chargePoint;
 
-        insertPointHistory(id, point, TransactionType.CHARGE);
+            chargePointException(chargePoint);
 
-        return userPointRepository.updatePointById(id, updatedPoint);
+            insertPointHistory(id, point, TransactionType.CHARGE);
+
+            return userPointRepository.updatePointById(id, updatedPoint);
+
+        }finally {
+            lock.unlock(); //락 해제
+        }
     }
 
 
     public UserPoint usePoint(long id, long point) {
-        long currentPoint = viewPoint(id).point();
-        long usePoint = point;
-        long updatedPoint = currentPoint - usePoint;
+        Lock lock = getLock(id);
+        lock.lock(); //락 획득
+        try{
+            long currentPoint = viewPoint(id).point();
+            long usePoint = point;
+            long updatedPoint = currentPoint - usePoint;
 
-        if(usePoint < 100 || usePoint > 10000) {
-            throw  new IllegalArgumentException("포인트 사용 최소값은 100 이상 10,000이하여야 합니다.");
+            if(usePoint < 100 || usePoint > 10000) {
+                throw  new IllegalArgumentException("포인트 사용 최소값은 100 이상 10,000이하여야 합니다.");
+            }
+
+            if(updatedPoint < 0){
+                throw new IllegalArgumentException("사용하려는 포인트가 잔고보다 많습니다.");
+            }
+
+            insertPointHistory(id, point, TransactionType.USE);
+
+            return userPointRepository.updatePointById(id, updatedPoint);
+
+        }finally {
+            lock.unlock(); //락 해제
         }
 
-        if(updatedPoint < 0){
-            throw new IllegalArgumentException("사용하려는 포인트가 잔고보다 많습니다.");
-        }
-
-        insertPointHistory(id, point, TransactionType.USE);
-
-        return userPointRepository.updatePointById(id, updatedPoint);
     }
 
     private static void chargePointException(long chargePoint) {
